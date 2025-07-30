52bbc54bd21a7a4a9a05596a86534a8a9ec89947
Junghyeon0710 <sie08357@naver.com>
2025-07-30 13:31:59 +0900

    feat: Frontend UI Subsystem 성성

---
+++ Source/CommonFront/Subsystems/FrontendUISubsystem.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "FrontendUISubsystem.generated.h"
+
+class UWidget_PrimaryLayout;
+/**
+ * 
+ */
+UCLASS()
+class COMMONFRONT_API UFrontendUISubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	static UFrontendUISubsystem* Get(const UObject* WorldContextObject);
+
+
+	/** USubsystem Interface */
+	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+	/** ~USubsystem Interface */
+
+	UFUNCTION(BlueprintCallable)
+	void RegisterCreatedPrimaryLayout(UWidget_PrimaryLayout* InCreateWidget);
+
+private:
+	UPROPERTY(Transient)
+	TObjectPtr<UWidget_PrimaryLayout> CreatedPrimaryLayout;
+};
