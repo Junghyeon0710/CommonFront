d38c31960a8378d86f3fb4ce6e4d6af7df9fd3df
Junghyeon0710 <sie08357@naver.com>
2025-07-31 17:57:53 +0900

    feat: 확인 화면 정보 클래스 설계

--- Source/CommonFront/Private/Widgets/Widget_ConfirmScreen.cpp
+++ Source/CommonFront/Private/Widgets/Widget_ConfirmScreen.cpp
@@ -3,3 +3,57 @@
 
 #include "Widgets/Widget_ConfirmScreen.h"
 
+UConfirmScreenInfoObject* UConfirmScreenInfoObject::CreateOkScreen(const FText& InScreenTitle, const FText& InScreenMsg)
+{
+	UConfirmScreenInfoObject* InfoObject = NewObject<UConfirmScreenInfoObject>();
+	InfoObject->ScreenTitle = InScreenTitle;
+	InfoObject->ScreenMessage = InScreenMsg;
+
+	FConfirmScreenButtonInfo OkButtonInfo;
+	OkButtonInfo.ConfirmScreenButtonType = EConfirmScreenButtonType::Closed;
+	OkButtonInfo.ButtonTextToDisplay = FText::FromString(TEXT("OK"));
+
+	InfoObject->AvailableScreenButtons.Add(OkButtonInfo);
+	
+	return InfoObject;
+}
+
+UConfirmScreenInfoObject* UConfirmScreenInfoObject::CreateYesNoScreen(const FText& InScreenTitle, const FText& InScreenMsg)
+{
+	UConfirmScreenInfoObject* InfoObject = NewObject<UConfirmScreenInfoObject>();
+	InfoObject->ScreenTitle = InScreenTitle;
+	InfoObject->ScreenMessage = InScreenMsg;
+
+	FConfirmScreenButtonInfo YesButtonInfo;
+	YesButtonInfo.ConfirmScreenButtonType = EConfirmScreenButtonType::Confirmed;
+	YesButtonInfo.ButtonTextToDisplay = FText::FromString(TEXT("Yes"));
+
+	FConfirmScreenButtonInfo NoButtonInfo;
+	NoButtonInfo.ConfirmScreenButtonType = EConfirmScreenButtonType::Cancelled;
+	NoButtonInfo.ButtonTextToDisplay = FText::FromString(TEXT("No"));
+
+	InfoObject->AvailableScreenButtons.Add(YesButtonInfo);
+	InfoObject->AvailableScreenButtons.Add(NoButtonInfo);
+	
+	return InfoObject;
+}
+
+UConfirmScreenInfoObject* UConfirmScreenInfoObject::CreateOkCancelScreen(const FText& InScreenTitle, const FText& InScreenMsg)
+{
+	UConfirmScreenInfoObject* InfoObject = NewObject<UConfirmScreenInfoObject>();
+	InfoObject->ScreenTitle = InScreenTitle;
+	InfoObject->ScreenMessage = InScreenMsg;
+
+	FConfirmScreenButtonInfo OKButtonInfo;
+	OKButtonInfo.ConfirmScreenButtonType = EConfirmScreenButtonType::Confirmed;
+	OKButtonInfo.ButtonTextToDisplay = FText::FromString(TEXT("OK"));
+
+	FConfirmScreenButtonInfo CancelButtonInfo;
+	CancelButtonInfo.ConfirmScreenButtonType = EConfirmScreenButtonType::Cancelled;
+	CancelButtonInfo.ButtonTextToDisplay = FText::FromString(TEXT("Cancel"));
+
+	InfoObject->AvailableScreenButtons.Add(OKButtonInfo);
+	InfoObject->AvailableScreenButtons.Add(CancelButtonInfo);
+
+	return InfoObject;
+}
--- Source/CommonFront/Public/Widgets/Widget_ConfirmScreen.h
+++ Source/CommonFront/Public/Widgets/Widget_ConfirmScreen.h
@@ -9,6 +9,39 @@
 
 class UDynamicEntryBox;
 class UCommonTextBlock;
+
+USTRUCT(BlueprintType)
+struct FConfirmScreenButtonInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EConfirmScreenButtonType ConfirmScreenButtonType = EConfirmScreenButtonType::UnKnown;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText ButtonTextToDisplay;
+};
+
+UCLASS()
+class COMMONFRONT_API UConfirmScreenInfoObject : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	static UConfirmScreenInfoObject* CreateOkScreen(const FText& InScreenTitle, const FText& InScreenMsg);
+	static UConfirmScreenInfoObject* CreateYesNoScreen(const FText& InScreenTitle, const FText& InScreenMsg);
+	static UConfirmScreenInfoObject* CreateOkCancelScreen(const FText& InScreenTitle, const FText& InScreenMsg);
+	
+	UPROPERTY(Transient)
+	FText ScreenTitle;
+
+	UPROPERTY(Transient)
+	FText ScreenMessage;
+
+	UPROPERTY(Transient)
+	TArray<FConfirmScreenButtonInfo> AvailableScreenButtons;
+};
+
 /**
  * 
  */
