0de8addcfbd19d374a7bbbf2f18306696650275a
Junghyeon0710 <sie08357@naver.com>
2025-08-01 16:10:58 +0900

    feat: 설정 화면 위젯 블프 추가

--- Source/CommonFront/FrontendGameplayTags.cpp
+++ Source/CommonFront/FrontendGameplayTags.cpp
@@ -9,6 +9,8 @@ namespace FrontEndGameplayTags
 
 	UE_DEFINE_GAMEPLAY_TAG(Frontend_Widget_PressAnyKeyScreen, "Frontend.Widget.PressAnyKeyScreen");
 	UE_DEFINE_GAMEPLAY_TAG(Frontend_Widget_MainMenuScreen, "Frontend.Widget.MainMenuScreen");
+	UE_DEFINE_GAMEPLAY_TAG(Frontend_Widget_OptionsScreen, "Frontend.Widget.OptionsScreen");
+	
 	UE_DEFINE_GAMEPLAY_TAG(Frontend_Widget_ConfirmScreen, "Frontend.Widget.ConfirmScreen");
 	
 	
---
+++ Source/CommonFront/Public/Widgets/Options/Widget_OptionsScreen.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/Widget_ActivatableBase.h"
+#include "Widget_OptionsScreen.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, BlueprintType, meta = (DisableNaiveTick))
+class COMMONFRONT_API UWidget_OptionsScreen : public UWidget_ActivatableBase
+{
+	GENERATED_BODY()
+	
+};
