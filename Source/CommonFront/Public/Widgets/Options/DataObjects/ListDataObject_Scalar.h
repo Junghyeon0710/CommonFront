5cfe9589b78da8f46694eccbaaf29628b7c93fb1
Junghyeon0710 <sie08357@naver.com>
2025-08-07 16:33:46 +0900

    feat: 스칼라 데이터 값 리셋 기능 추가

--- Source/CommonFront/Public/Widgets/Options/DataObjects/ListDataObject_Scalar.h
+++ Source/CommonFront/Public/Widgets/Options/DataObjects/ListDataObject_Scalar.h
@@ -27,7 +27,11 @@ public:
 
 	float GetCurrentValue() const;
 	void SetCurrentValueFromSlider(float InNewValue);
+	
 private:
+	virtual bool CanResetBackToDefaultValue() const override;
+	virtual bool TryResetBackToDefaultValue() override;
+	
 	float StringToFloat(const FString& InString) const;
 	
 	TRange<float> DisplayValueRange = TRange<float>(0.1f, 0.1f);
