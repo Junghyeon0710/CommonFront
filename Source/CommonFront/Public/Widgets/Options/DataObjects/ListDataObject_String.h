0a6abe16190eaa03cbed17e9fd917a5b900905cc
Junghyeon0710 <sie08357@naver.com>
2025-08-07 17:34:39 +0900

    feat: Stringbool ObjectData 추가

--- Source/CommonFront/Public/Widgets/Options/DataObjects/ListDataObject_String.h
+++ Source/CommonFront/Public/Widgets/Options/DataObjects/ListDataObject_String.h
@@ -36,3 +36,25 @@ public:
 	FORCEINLINE const TArray<FText>& GetAvailableOptionsTextArray() const { return AvailableOptionsTextArray; }
 	FORCEINLINE FText GetCurrentDisplayText() const { return CurrentDisplayText; }
 };
+
+UCLASS()
+class COMMONFRONT_API UListDataObject_StringBool : public UListDataObject_String
+{
+	GENERATED_BODY()
+
+public:
+	void OverrideTrueDisplayText(const FText& InNewTrueDisplayText);
+	void OverrideFalseDisplayText(const FText& InNewFalseDisplayText);
+	void SetTrueAsDefaultValue();
+	void SetFalseAsDefaultValue();
+
+protected:
+	virtual void OnDataObjectInitialized() override;
+	
+private:
+	void TryInitBoolValues();
+	
+	const FString TrueString = TEXT("true");
+	const FString FalseString = TEXT("false");
+};
+
