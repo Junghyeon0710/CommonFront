7377c912fc7ffb896f2a1080bb6b036e84773982
Junghyeon0710 <sie08357@naver.com>
2025-08-08 12:41:45 +0900

    feat: 해상기능 적용

--- Source/CommonFront/Private/Widgets/Options/DataObjects/ListDataObject_StringResolution.cpp
+++ Source/CommonFront/Private/Widgets/Options/DataObjects/ListDataObject_StringResolution.cpp
@@ -4,26 +4,48 @@
 #include "Widgets/Options/DataObjects/ListDataObject_StringResolution.h"
 
 #include "CommonFront/FrontendDebugHelper.h"
+#include "FrontendSettings/FrontendGameUserSettings.h"
 #include "Kismet/KismetSystemLibrary.h"
 
 void UListDataObject_StringResolution::InitResolutionValues()
 {
-	TArray<FIntPoint> AvailableResolutions ;
+	TArray<FIntPoint> AvailableResolutions;
 	
-	UKismetSystemLibrary::GetSupportedFullscreenResolutions(AvailableResolutions );
+	UKismetSystemLibrary::GetSupportedFullscreenResolutions(AvailableResolutions);
+
+	AvailableResolutions.Sort([](const FIntPoint& A, const FIntPoint& B)
+	{
+		return A.SizeSquared() < B.SizeSquared();
+	});
 
 	for (const FIntPoint& Resolution : AvailableResolutions)
 	{
-		Debug::Print(TEXT("Available Resolutions: ") + Resolution.ToString());
+		AddDynamicOptions(ResToValueString(Resolution), ResToDisplayText(Resolution));
 	}
+
+	MaximumAllowedResolution = ResToValueString(AvailableResolutions.Last());
+
+	SetDefaultValueFromString(MaximumAllowedResolution);
 }
 
 void UListDataObject_StringResolution::OnDataObjectInitialized()
 {
 	Super::OnDataObjectInitialized();
 
-	if (DataDynamicGetter)
+	if (!TrySetDisplayTextFromStringValue(CurrentStringValue))
 	{
-		Debug::Print(TEXT("UResolution Value from Dynamic Getter: ") + DataDynamicGetter->GetValueAsString());
+		CurrentDisplayText = ResToDisplayText(UFrontendGameUserSettings::Get()->GetScreenResolution());
 	}
+}
+
+FString UListDataObject_StringResolution::ResToValueString(const FIntPoint& InResolution) const
+{
+	return FString::Printf(TEXT("(X=%i,Y=%i)"), InResolution.X, InResolution.Y);
+}
+
+FText UListDataObject_StringResolution::ResToDisplayText(const FIntPoint& InResolution) const
+{
+	const FString DisplayString = FString::Printf(TEXT("%i x %i"), InResolution.X, InResolution.Y);
+
+	return FText::FromString(DisplayString);
 }
