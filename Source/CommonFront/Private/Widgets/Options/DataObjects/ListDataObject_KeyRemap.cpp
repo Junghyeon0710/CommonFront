c2c1c57ff2e137aa50ce2576f3d4514c09a341c2
Junghyeon0710 <sie08357@naver.com>
2025-08-11 16:18:31 +0900

    feat: Key Remap Data Object 클래스 추가

---
+++ Source/CommonFront/Private/Widgets/Options/DataObjects/ListDataObject_KeyRemap.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/Options/DataObjects/ListDataObject_KeyRemap.h"
+
+void UListDataObject_KeyRemap::InitKeyRemapData(UEnhancedInputUserSettings* InOwningInputUserSettings, UEnhancedPlayerMappableKeyProfile* InKeyProfile, ECommonInputType InDesiredInputKeyType, const FPlayerKeyMapping& InOwningPlayerKeyMapping)
+{
+	CachedOwningInputUserSettings = InOwningInputUserSettings;
+	CachedOwningKeyProfile = InKeyProfile;
+	CachedDesiredInputType = InDesiredInputKeyType;
+	CachedOwningMappingName = InOwningPlayerKeyMapping.GetMappingName();
+	CachedOwningMappableKeySlot = InOwningPlayerKeyMapping.GetSlot();
+}
